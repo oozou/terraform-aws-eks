19c04ea40992aac3e9ef41275940dbfd9cd3138c
waruwat <mai@oozou.com>
2022-03-22 19:59:15 +0700

    (docs) terraform fmt

--- bootstrap.tf
+++ bootstrap.tf
@@ -1,19 +1,19 @@
 module "bootstrap" {
-  source                   = "./modules/bootstrap"
-  subnet_id                = var.subnets_ids[0]
-  cluster_name             = aws_eks_cluster.this.name
-  aws_account              = var.aws_account
-  admin_role_arn           = var.admin_role_arn
-  dev_role_arn             = var.dev_role_arn
-  readonly_role_arn        = var.readonly_role_arn
-  node_group_role_arn      = aws_iam_role.node_group_role.arn
-  oidc_arn                 = aws_iam_openid_connect_provider.this.arn
-  vpc_id                   = var.vpc_id
+  source                      = "./modules/bootstrap"
+  subnet_id                   = var.subnets_ids[0]
+  cluster_name                = aws_eks_cluster.this.name
+  aws_account                 = var.aws_account
+  admin_role_arn              = var.admin_role_arn
+  dev_role_arn                = var.dev_role_arn
+  readonly_role_arn           = var.readonly_role_arn
+  node_group_role_arn         = aws_iam_role.node_group_role.arn
+  oidc_arn                    = aws_iam_openid_connect_provider.this.arn
+  vpc_id                      = var.vpc_id
   is_config_aws_auth          = var.is_config_aws_auth
   is_config_aws_lb_controller = var.is_config_aws_lb_controller
-  is_config_argo_cd        = var.is_config_argo_cd
-  acm_arn                  = var.acm_arn
-  argo_cd_domain           = var.argo_cd_domain
-  prefix                   = local.prefix
-  tags                     = local.tags
+  is_config_argo_cd           = var.is_config_argo_cd
+  acm_arn                     = var.acm_arn
+  argo_cd_domain              = var.argo_cd_domain
+  prefix                      = local.prefix
+  tags                        = local.tags
 }
