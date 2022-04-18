41a476d81efd6803219e708794b98d49a1f1af89
waruwat <mai@oozou.com>
2022-04-18 11:28:57 +0700

    (feat) change default password for argo

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_eks_cluster" "this" {
+  name     = "${local.prefix}-cluster"
+  role_arn = aws_iam_role.cluster_role.arn
+
+  vpc_config {
+    endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
+    subnet_ids              = var.subnets_ids
+    security_group_ids      = [aws_security_group.cluster.id]
+  }
+  version = var.eks_version
+  tags = merge(
+    {
+      "Name" = "${local.prefix}-cluster"
+    },
+    local.tags
+  )
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_cluster_policy,
+    aws_iam_role_policy_attachment.amazon_eks_vpc_resource_controller,
+  ]
+}
