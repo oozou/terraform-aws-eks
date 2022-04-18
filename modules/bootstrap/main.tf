59402dbe32551d005599ffdf65961b352e3013b2
waruwat <mai@oozou.com>
2022-04-18 11:47:04 +0700

    (feat) change version ec2 module

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source       = "git::ssh://git@github.com/oozou/terraform-aws-ec2-instance.git?ref=feature_bootstap"
+  source       = "git::ssh://git@github.com/oozou/terraform-aws-ec2-instance.git?ref=v1.0.0"
   prefix       = var.prefix
   environment  = var.environment
   ami          = data.aws_ami.ubuntu.id
