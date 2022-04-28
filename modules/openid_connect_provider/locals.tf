90dc740ed5a045eaf9c78f85423e40df1de99d36
waruwat <mai@oozou.com>
2022-04-28 09:52:42 +0700

    (feat) change sequence service account

--- modules/openid_connect_provider/locals.tf
+++ modules/openid_connect_provider/locals.tf
@@ -8,9 +8,10 @@ locals {
     name                 = "argo-cd-image-updater"
     existing_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"]
   }
-  service_accounts = concat(var.additional_service_accounts,
+  service_accounts = concat(
     var.is_create_loadbalancer_controller_sa ? [local.loadbalancer_controller_sa] : [],
-    var.is_create_argo_image_updater_sa ? [local.argo_image_updater_sa] : []
+    var.is_create_argo_image_updater_sa ? [local.argo_image_updater_sa] : [],
+    var.additional_service_accounts
   )
 
 }
