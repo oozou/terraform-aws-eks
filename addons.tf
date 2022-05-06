4d5e836189fddaad8d39ebdc789416885c87e9e5
waruwat <mai@oozou.com>
2022-05-06 17:44:13 +0700

    (docs) format terraform

--- addons.tf
+++ addons.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_addon" "vpc_cni" {
-  count = length(var.additional_addons)
+  count        = length(var.additional_addons)
   cluster_name = aws_eks_cluster.this.name
   addon_name   = var.additional_addons[count.index]
 }
