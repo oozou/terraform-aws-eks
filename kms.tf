c68af02280b2952cdd08a91cec9c6a90577c5a60
waruwat <mai@oozou.com>
2022-07-12 10:36:29 +0700

    (feat) change version of kms

--- kms.tf
+++ kms.tf
@@ -1,7 +1,7 @@
 module "eks_kms" {
   count = var.is_enabled_cluster_encryption ? 1 : 0
 
-  source      = "git@github.com:oozou/terraform-aws-kms-key.git?ref=v0.0.2"
+  source      = "git@github.com:oozou/terraform-aws-kms-key.git?ref=v1.0.0"
   key_type    = "service"
   description = "Used to encrypt data for eks secret"
   prefix      = var.prefix
