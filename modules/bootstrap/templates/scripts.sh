2e39eaf3d63aab81c8f43454b856a8b71faee919
Brian - oozou <98243528+lycbrian@users.noreply.github.com>
2023-06-16 12:41:53 +0700

    fix: update bootstrap scripts.sh

--- modules/bootstrap/templates/scripts.sh
+++ modules/bootstrap/templates/scripts.sh
@@ -3,7 +3,10 @@
 echo "starting cloud init script . . ."
 sudo su
 sudo apt-get update
-sudo apt install awscli -y
+sudo apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 sudo apt install jq -y
 
 # kubectl
@@ -12,7 +15,9 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubectl=1.23.4-00
+#sudo apt-get install -y kubectl=1.23.4-00
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # create file
 echo "create script folder . . ."
@@ -34,5 +39,5 @@ sudo chmod 777 /opt/scripts/eks-manifest-file.yml
 sudo echo '${eks_manifest_file}' > /opt/scripts/eks-manifest-file.yml
 sudo AWS_ACCESS_KEY_ID=$aws_access_key_id AWS_SECRET_ACCESS_KEY=$aws_secret_access_key kubectl apply -f /opt/scripts/eks-manifest-file.yml
 %{ endif }
-
+kubectl set env daemonset aws-node -n kube-system ENABLE_PREFIX_DELEGATION=true
 sudo shutdown -h now
