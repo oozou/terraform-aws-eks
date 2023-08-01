a7d55af06400eee981d241b73cc95b6125670014
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2023-08-01 15:42:39 +0700

    chore: update url

--- modules/bootstrap/templates/scripts.sh
+++ modules/bootstrap/templates/scripts.sh
@@ -11,18 +11,22 @@ sudo apt install jq -y
 
 # kubectl
 echo "install kubectl . . ."
-sudo apt-get install -y apt-transport-https ca-certificates curl
-# # sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-# # echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+## sudo apt-get install -y apt-transport-https ca-certificates curl
+## sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+## echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo mkdir -p /etc/apt/keyrings
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
-
+# New update
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates
+curl -4 -s https://dl.k8s.io/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
+sudo apt-get install -y kubectl=1.23.4-00
+
+## sudo apt-get update
 #sudo apt-get install -y kubectl=1.23.4-00
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+## curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+## sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # create file
 echo "create script folder . . ."
