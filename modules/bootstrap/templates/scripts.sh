34f88ed35da863bdf4d222f994ce09d45614a668
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2023-08-02 13:51:50 +0700

    chore: update comment section

--- modules/bootstrap/templates/scripts.sh
+++ modules/bootstrap/templates/scripts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 # install dependencies packages
-echo "starting cloud init script . . ."
+echo "/* -------------------------------------------------------------------------- */"
+echo "/*                         starting cloud init script                         */"
+echo "/* -------------------------------------------------------------------------- */"
 sudo su
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
 sudo apt-get update
@@ -11,17 +13,23 @@ sudo ./aws/install --update
 sudo apt install jq -y
 
 # kubectl
-echo "install kubectl . . ."
+echo "/* -------------------------------------------------------------------------- */"
+echo "/*                               install kubectl                              */"
+echo "/* -------------------------------------------------------------------------- */"
 curl -LO https://dl.k8s.io/release/v1.27.4/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
 # create file
-echo "create script folder . . ."
+echo "/* -------------------------------------------------------------------------- */"
+echo "/*                            create script folder                            */"
+echo "/* -------------------------------------------------------------------------- */"
 sudo mkdir -p /opt/scripts
 
 # configure aws
-echo "config aws account . . ."
+echo "/* -------------------------------------------------------------------------- */"
+echo "/*                             config aws account                             */"
+echo "/* -------------------------------------------------------------------------- */"
 aws configure set region ${region}
 credential=$(aws secretsmanager get-secret-value  --secret-id ${eks_bootstrap_secret_arn} --query SecretString --output text)
 aws_access_key_id=$(echo $credential | jq '.aws_access_key_id' | tr -d '"')
@@ -30,11 +38,16 @@ export AWS_ACCESS_KEY_ID=$aws_access_key_id
 export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
 aws eks update-kubeconfig --region ${region} --name ${cluster_name}
 %{ if is_config_aws_auth }
-echo "config aws-auth . . ."
+echo "/* -------------------------------------------------------------------------- */"
+echo "/*                               config aws-auth                              */"
+echo "/* -------------------------------------------------------------------------- */"
 sudo touch /opt/scripts/eks-manifest-file.yml
 sudo chmod 777 /opt/scripts/eks-manifest-file.yml
 sudo echo '${eks_manifest_file}' > /opt/scripts/eks-manifest-file.yml
 sudo AWS_ACCESS_KEY_ID=$aws_access_key_id AWS_SECRET_ACCESS_KEY=$aws_secret_access_key kubectl apply -f /opt/scripts/eks-manifest-file.yml
 %{ endif }
+echo "/* -------------------------------------------------------------------------- */"
+echo "/*                              set env daemonset                             */"
+echo "/* -------------------------------------------------------------------------- */"
 kubectl set env daemonset aws-node -n kube-system ENABLE_PREFIX_DELEGATION=true
 sudo shutdown -h now
