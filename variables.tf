c43d8d28159022392200b4c4b9599b4fd8904d47
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2023-08-02 13:22:42 +0700

    test: support additional clusterole and clsuterrolebinding

--- variables.tf
+++ variables.tf
@@ -131,6 +131,65 @@ variable "readonly_role_arns" {
   default     = []
 }
 
+variable "additional_cluster_role" {
+  description = <<EOL
+Additional cluster role resource
+additional_cluster_role = [
+  {
+    name = "cluster_role_name"
+    rules = [
+      { # Workloads
+        apiGroups = ["*"]
+        resources = ["pods", "deployments", "replicasets"]
+        verbs     = ["get", "list", "watch", "create", "update", "delete", "patch"]
+      },
+      { # Config
+        apiGroups = ["*"]
+        resources = ["configmaps", "secrets", "horizontalpodautoscalers"]
+        verbs     = ["get", "list", "watch", "create", "update", "delete", "patch"]
+      },
+      { # Network
+        apiGroups = ["*"]
+        resources = ["services", "ingresses"]
+        verbs     = ["get", "list", "watch", "create", "update", "delete", "patch"]
+    ]
+  }
+]
+EOL
+  type        = any
+  default     = []
+}
+
+variable "additional_cluster_role_binding" {
+  description = <<EOL
+Additional cluster role resource
+additional_cluster_role_binding = [
+  {
+    name = "bdd"
+    subjects = [
+      {
+        kind     = "User"
+        name     = "role"
+        apiGroup = "rbac.authorization.k8s.io"
+      },
+      {
+        kind     = "User"
+        name     = "role-x"
+        apiGroup = "rbac.authorization.k8s.io"
+      }
+    ]
+    roleRef = {
+      apiGroup = "rbac.authorization.k8s.iox"
+      kind     = "ClusterRole"
+      name     = "devops-clusterrole"
+    }
+  }
+]
+EOL
+  type        = any
+  default     = []
+}
+
 variable "admin_iam_arns" {
   description = "admin iam arns for grant permission to aws-auth"
   type        = list(string)
